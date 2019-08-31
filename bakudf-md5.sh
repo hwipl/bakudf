e5502bcc2ef49d93142364540d09a6a93c1dd876
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-31 13:24:25 +0200

    bakudf-md5.sh: cleanups
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- bakudf-md5.sh
+++ bakudf-md5.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-#truncate -s 25GB "$1".udf
-#mkudffs "$1".udf
-#
-##mkudffs --media-type=dvd /tmp/cdimage.udf
-#mkudffs --label="$1" "$1".udf
-#
-#sudo mkdir "$1"
-#sudo mount -t udf -o loop,rw "$1".udf "$1"
-
-
 if [[ "$1" == "create" ]]; then
 	cd "$2"
 	find . -type f -exec md5sum {} \;>> ../"$2".md5
@@ -21,4 +11,3 @@ if [[ "$1" == "verify" ]]; then
 	echo "Verifying $2 in $3"
 	md5sum -c "$MD5DIR"/"$2".md5
 fi
-
