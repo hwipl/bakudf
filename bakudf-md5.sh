3d529906eb8f2e60b691c9fc6a838486ead40f35
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-31 13:39:21 +0200

    bakudf-md5.sh: use variable names and add some comments
    Use variable names instead of the command line arguments directly. Also,
    add some comments explaining the different sections in the script.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- bakudf-md5.sh
+++ bakudf-md5.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-if [[ "$1" == "create" ]]; then
-	cd "$2"
-	find . -type f -exec md5sum {} \;>> ../"$2".md5
+CMD=$1
+DIR=$2
+MD5=$3
+
+# optional absolute path to a md5 file
+if [[ -z "$MD5" ]]; then
+	MD5=$(pwd)"/$DIR".md5
 fi
 
-if [[ "$1" == "verify" ]]; then
-	MD5DIR=$(pwd)
-	cd "$3"
-	echo "Verifying $2 in $3"
-	md5sum -c "$MD5DIR"/"$2".md5
+# create a new md5 file for all files in a directory
+if [[ "$CMD" == "create" ]]; then
+	cd "$DIR" || exit
+	find . -type f -exec md5sum {} \;>> ../"$DIR".md5
+fi
+
+# verify files in a directory with md5 file
+if [[ "$CMD" == "verify" ]]; then
+	cd "$DIR" || exit
+	echo "Verifying $(pwd) with $MD5"
+	md5sum -c "$MD5"
 fi
