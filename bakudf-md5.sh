b537dc252109547c3af17c95d20712f3128b53e4
hwipl <33433250+hwipl@users.noreply.github.com>
2019-08-31 13:51:07 +0200

    bakudf-md5.sh: add usage
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- bakudf-md5.sh
+++ bakudf-md5.sh
@@ -4,6 +4,18 @@ CMD=$1
 DIR=$2
 MD5=$3
 
+# usage
+if [[ "$#" -lt 2 ]]; then
+	echo "Usage:"
+	echo "$0 <create|verify> <dir> [md5]"
+	echo "Arguments:"
+	echo "    create: create a new md5 file for all files in \"dir\""
+	echo "    verify: verify files in directory \"dir\" with md5 file"
+	echo "    dir:    directory containing files for md5 file"
+	echo "    md5:    absolute path to an alternative md5 file to verify"
+	exit
+fi
+
 # optional absolute path to a md5 file
 if [[ -z "$MD5" ]]; then
 	MD5=$(pwd)"/$DIR".md5
