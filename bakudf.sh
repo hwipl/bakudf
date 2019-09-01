96ff5a9e57ee59162127c31ec37705d9320f66b3
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-01 12:38:04 +0200

    bakudf.sh: add finalize command line argument
    Add finalize command line argument for unmounting the udf file system
    and printing next steps.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- bakudf.sh
+++ bakudf.sh
@@ -5,22 +5,26 @@ TRUNCATE=/usr/bin/truncate
 MKUDFFS=/usr/bin/mkudffs
 SUDO=/usr/bin/sudo
 MOUNT=/usr/bin/mount
+UMOUNT=/usr/bin/umount
 
 # output text
 STEPS_AFTER_CREATE="Next steps:
-* Copy all files to the folder \"$PROJECT\".
-* Optional: create an md5 checksum file for the files in \"$PROJECT\" with
-  ./bakudf-md5.sh create $PROJECT
-  and copy \"$PROJECT.md5\" into the \"$PROJECT\" folder.
-* Unmount the folder with \"umount $PROJECT\".
+* Copy all files to the folder \"%s\".
+* Optional: create an md5 checksum file for the files in \"%s\" with
+  ./bakudf-md5.sh create %s
+  and copy \"%s.md5\" into the \"%s\" folder."
+
+STEPS_AFTER_FINALIZE="Next steps:
 * Burn the udf file to a disc, e.g., with:
-  cdrecord -v -doa driveropts=burnfree dev=/dev/sr0 $PROJECT.udf
-  (Replace /dev/sr0 with the path to your device)."
+  cdrecord -v -doa driveropts=burnfree dev=/dev/sr0 %s.udf
+  (Replace /dev/sr0 with the path to your device).
+* You can remove the project folder \"%s\" and udf file \"%s.udf\""
 
 # print usage
 function print_usage {
 	echo "Usage:"
 	echo "    $0 <create> <project> <size>"
+	echo "    $0 <finalize> <project>"
 	echo "Arguments:"
 	echo "    project: name of the new project"
 	echo "    size:    expected size of the project, e.g., 25GB, 5GB"
@@ -61,10 +65,33 @@ function create_project {
 	$SUDO $MOUNT -t udf -o loop,rw "$PROJECT".udf "$PROJECT"
 
 	# give further instructions
-	echo "$STEPS_AFTER_CREATE"
+	echo "${STEPS_AFTER_CREATE//%s/$PROJECT}"
+}
+
+# finalize a project
+function finalize_project {
+	# arguments
+	PROJECT=$1
+
+	# check arguments
+	if [[ "$#" -lt 1 ]]; then
+		print_usage
+		exit
+	fi
+
+	# umount udf file
+	$SUDO $UMOUNT "$PROJECT"
+
+	# give further instructions
+	echo "${STEPS_AFTER_FINALIZE//%s/$PROJECT}"
 }
 
 if [[ "$1" == "create" ]]; then
 	shift
 	create_project "$@"
+elif [[ "$1" == "finalize" ]]; then
+	shift
+	finalize_project "$@"
+else
+	print_usage
 fi
