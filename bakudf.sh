4f09b37293bc4e0394d977d636e3a5d936824345
hwipl <33433250+hwipl@users.noreply.github.com>
2019-09-01 11:50:09 +0200

    bakudf.sh: move project creation into function
    Move creation of a new project into a function.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- bakudf.sh
+++ bakudf.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-PROJECT=$1
-SIZE=$2
-
 # other program locations
 TRUNCATE=/usr/bin/truncate
 MKUDFFS=/usr/bin/mkudffs
 MOUNT=/usr/bin/mount
+
+# output text
+STEPS_AFTER_CREATE="Next steps:
+* Copy all files to the folder \"$PROJECT\".
+* Optional: create an md5 checksum file for the files in \"$PROJECT\" with
+  ./bakudf-md5.sh create $PROJECT
+  and copy \"$PROJECT.md5\" into the \"$PROJECT\" folder.
+* Unmount the folder with \"umount $PROJECT\".
+* Burn the udf file to a disc, e.g., with:
+  cdrecord -v -doa driveropts=burnfree dev=/dev/sr0 $PROJECT.udf
+  (Replace /dev/sr0 with the path to your device)."
 
 # print usage
 function print_usage {
@@ -17,40 +25,42 @@ function print_usage {
 	echo "    size:    expected size of the project, e.g., 25GB, 5GB"
 }
 
-if [[ "$#" -lt 2 ]]; then
-	print_usage
-	exit
-fi
+# create new project
+function create_project {
+	# arguments
+	PROJECT=$1
+	SIZE=$2
 
-# check if files/folders exist and abort in that case
-FILES="$PROJECT.udf $PROJECT"
-for i in $FILES; do
-	if [[ -e "$i" ]]; then
-		echo "$i already exists."
+	# check arguments
+	if [[ "$#" -lt 2 ]]; then
+		print_usage
 		exit
 	fi
-done
 
-# create udf file for project
-echo "Creating a new udf file: $PROJECT.udf"
-$TRUNCATE -s "$SIZE" "$PROJECT".udf
+	# check if files/folders exist and abort in that case
+	FILES="$PROJECT.udf $PROJECT"
+	for i in $FILES; do
+		if [[ -e "$i" ]]; then
+			echo "$i already exists."
+			exit
+		fi
+	done
 
-# create udf file system in udf file
-echo "Creating a new udf file system in $PROJECT.udf"
-$MKUDFFS --label="$PROJECT" "$PROJECT".udf > /dev/null
+	# create udf file for project
+	echo "Creating a new udf file: $PROJECT.udf"
+	$TRUNCATE -s "$SIZE" "$PROJECT".udf
 
-# mount udf file
-echo "Mounting $PROJECT.udf to the folder \"$PROJECT\""
-mkdir "$PROJECT"
-sudo $MOUNT -t udf -o loop,rw "$PROJECT".udf "$PROJECT"
+	# create udf file system in udf file
+	echo "Creating a new udf file system in $PROJECT.udf"
+	$MKUDFFS --label="$PROJECT" "$PROJECT".udf > /dev/null
 
-# give further instructions
-echo "Next steps:
-* Copy all files to the folder \"$PROJECT\".
-* Optional: create an md5 checksum file for the files in \"$PROJECT\" with
-  ./bakudf-md5.sh create $PROJECT
-  and copy \"$PROJECT.md5\" into the \"$PROJECT\" folder.
-* Unmount the folder with \"umount $PROJECT\".
-* Burn the udf file to a disc, e.g., with:
-  cdrecord -v -doa driveropts=burnfree dev=/dev/sr0 $PROJECT.udf
-  (Replace /dev/sr0 with the path to your device)."
+	# mount udf file
+	echo "Mounting $PROJECT.udf to the folder \"$PROJECT\""
+	mkdir "$PROJECT"
+	sudo $MOUNT -t udf -o loop,rw "$PROJECT".udf "$PROJECT"
+
+	# give further instructions
+	echo "$STEPS_AFTER_CREATE"
+}
+
+create_project "$@"
